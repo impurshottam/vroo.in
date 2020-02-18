a8c5bc1ee4a84232e919bb4ab5d2fb551da42c45
Purshottam <purshottam.indore@gmail.com>
2020-02-18 12:29:40 +0530

    Page not found done

--- imports/ui/views/shared/PageNotFound.jsx
+++ imports/ui/views/shared/PageNotFound.jsx
@@ -2,16 +2,43 @@ import React, {Component} from 'react';
 import {withStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import {compose} from 'redux';
-import Header from './Header';
-import Footer from './Footer';
-const styles = theme => ({root: {}});
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        justifyContent:'center',
+        backgroundColor:'rgb(232, 234, 237)'
+    },
+    main: {
+        marginTop: theme.spacing(8),
+        marginBottom: theme.spacing(2),
+        align:'center'
+        // outline:'1px solid red'
+    },
+});
 class PageNotFound extends Component {
     render() {
         const {classes} = this.props;
         return (
-            <div>
-                <Header/>
-                <Footer/>
+            <div className={classes.root}>
+                <CssBaseline/>
+                <Container component="main" className={classes.main} maxWidth="sm">
+                    <div style={{textAlign:'center'}}>   
+                        <ErrorOutlineIcon fontSize="large"/>                        
+                    </div>
+                    <Typography align="center" variant="h6" component="h1" gutterBottom>
+                        404 Error
+                    </Typography>
+                    <Typography align="center" variant="body1" component="h1" gutterBottom>
+                        {'Opps! Page does not found.'}
+                    </Typography>
+                </Container>
             </div>
         )
     }
