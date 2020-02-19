a3ae5852f4b783ed979177cfd4d27aa990ddad65
Purshottam <purshottam.indore@gmail.com>
2020-02-19 09:09:43 +0530

    Home page done

--- imports/ui/views/home/Home.jsx
+++ imports/ui/views/home/Home.jsx
@@ -1,14 +1,112 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {withStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import {compose} from 'redux';
-const styles = theme => ({root: {}});
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import 'aframe';
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh'
+    },
+    content: {
+        height: 'calc(100vh - 64px)',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        zIndex: 1
+    },
+    navbar: {
+        borderBottom: `1px solid ${theme.palette.divider}`
+    },
+    gap: {
+        flex: 1
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    button: {
+        margin: theme.spacing(3, 0, 2)
+    },
+    layer: {
+        width: '100%',
+        height: 'calc(100vh - 64px)',
+        position: 'absolute',
+        marginTop: '64px'
+    }
+});
 class Home extends Component {
     render() {
         const {classes} = this.props;
         return (
-            <div>
-                <h1>Home</h1>
+            <div className={classes.root}>
+                <Toolbar className={classes.navbar}>
+                    <img src="images/vroo-black.png" width="80px"/>
+                    <span className={classes.gap}></span>
+                    <Button variant="outlined" size="small">
+                        Sign In
+                    </Button>
+                </Toolbar>
+
+                <a-scene vr-mode-ui="enabled: false">
+                    <a-assets>
+                        <img id="sky" src="images/360/sample1.jpg"/>
+                    </a-assets>
+                    <a-sky src="#sky"></a-sky>
+                    <a-entity
+                        rotation="0 0 0"
+                        animation="property: rotation; to: 0 360 0; loop: true; dur: 50000">
+                        <a-camera></a-camera>
+                    </a-entity>
+                </a-scene>
+                <div className={classes.layer}></div>
+                <Container className={classes.content} component="main" maxWidth="lg">
+                    <CssBaseline/>
+                    <div className={classes.paper}>
+                        <img width="120" src="images/vroo-white-small.png"/>
+                        <Typography
+                            component="h1"
+                            variant="h3"
+                            style={{
+                            color: 'white'
+                        }}>
+                            Making Virtual Tours Simple.
+                        </Typography>
+                        <Typography
+                            style={{
+                            color: 'white'
+                        }}
+                            align="left"
+                            variant="subtitle2"
+                            >
+                            VROO Is India's First 360 Virtual Reality Tour Online Publishing Platform.
+                        </Typography>
+                        {/* <Typography
+                            style={{
+                            color: 'white'
+                        }}
+                            align="center"
+                            variant="h6"
+                            >
+                            Click. Upload. Publish.
+                        </Typography> */}
+                        <Button
+                            type="button"
+                            variant="contained"
+                            color="primary"
+                            className={classes.button}>
+                            Get Started
+                        </Button>
+                    </div>
+                </Container>
             </div>
         )
     }
