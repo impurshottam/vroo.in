742a54344997892b3b658e628b32cf813f3c91c8
Purshottam <purshottam.indore@gmail.com>
2020-03-07 11:23:48 +0530

    .

---
+++ imports/startup/server/uploads.js
@@ -0,0 +1,28 @@
+Slingshot.fileRestrictions("UsersAvatar", {
+    allowedFileTypes: ["image/png", "image/jpeg", "image/jpg"],
+    maxSize: 5 * 500 * 500 // 2 MB (use null for unlimited)
+  });
+  
+  
+  Slingshot.createDirective("UsersAvatar", Slingshot.S3Storage, {
+    bucket: "this-app-2020", // change this to your s3's bucket name
+  
+    acl: "public-read",
+  
+    authorize: function (file, metaContext) {
+      
+      //Deny uploads if user is not logged in.
+      if (!this.userId) {
+        var message = "Please login before posting files";
+        throw new Meteor.Error("Login Required", message);
+      }
+  
+      return true;
+    },
+  
+    key: function (file, metaContext) {
+      // User's image url with ._id attached:
+  
+      return metaContext.avatarId + "/" + Date.now() + "-" + file.name;
+    }
+  });
